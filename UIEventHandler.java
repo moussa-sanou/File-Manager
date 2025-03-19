52177b3751d19c5fb59c28bae008003be47a7c93
samidollars <muss.junior@gmail.com>
2025-03-19 00:27:27 -0500

    Project: File Manager Complete project

---
+++ UIEventHandler.java
@@ -0,0 +1,7 @@
+
+public interface UIEventHandler {
+
+    public void onList(String path);
+    public void onCreate(String path);
+    public void onDelete(String path);
+}
---
+++ UserInterface.java
@@ -0,0 +1,6 @@
+
+public interface UserInterface {
+    public void start();
+    public void subscribe(UIEventHandler handler);
+    public void display(String message);
+}
